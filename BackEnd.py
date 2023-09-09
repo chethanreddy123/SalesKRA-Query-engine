58b279f150d791eca8b1fcad9d9779372ba0a37b
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-09-09 15:09:00 +0530

    first commit

---
+++ BackEnd.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException, Request, Response
+
+import google.generativeai as palm
+palm.configure(api_key='AIzaSyA1fu-ob27CzsJozdr6pHd96t5ziaD87wM')
+
+app = FastAPI()
+
+response = palm.chat(messages=["Jon is very good boy and works and microsoft"])
+
+@app.post("/chat/")
+async def chat(request: Request):
+    data = await request.json()
+    message = data.get("message")
+    
+    if message is None:
+        raise HTTPException(status_code=400, detail="Message not provided")
+
+    global response
+    if message.upper() == "Q":
+        return {"response": "Goodbye!"}
+
+    response = response.reply(message)
+    return {"response": response.last}
